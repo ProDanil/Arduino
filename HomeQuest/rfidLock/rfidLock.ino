de3b3e079972cb1e228411e3e94373c7519db138
ProDanil <43303306+ProDanil@users.noreply.github.com>
2023-05-17 13:37:22 +0300

    Update rfidLock.ino

--- HomeQuest/rfidLock/rfidLock.ino
+++ HomeQuest/rfidLock/rfidLock.ino
@@ -65,19 +65,19 @@ void lock(){
 
 
 void setup() {
-	Serial.begin(9600);		                   // Initialize serial communications with the PC
+  Serial.begin(9600);		                   // Initialize serial communications with the PC
 
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(RED_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
 
-	while (!Serial);		                     // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-	SPI.begin();			                       // Init SPI bus
+  while (!Serial);		                     // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();			                       // Init SPI bus
 
-	mfrc522.PCD_Init();		                   // Init MFRC522
-	delay(4);				                         // Optional delay. Some board do need more time after init to be ready, see Readme
-	mfrc522.PCD_DumpVersionToSerial();	     // Show details of PCD - MFRC522 Card Reader details
-	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+  mfrc522.PCD_Init();		                   // Init MFRC522
+  delay(4);				                         // Optional delay. Some board do need more time after init to be ready, see Readme
+  mfrc522.PCD_DumpVersionToSerial();	     // Show details of PCD - MFRC522 Card Reader details
+  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 }
 
 
